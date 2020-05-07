f454497d6e9926549c9f0d482d61d0488cd92434
utkarsh8021 <mishra8021@gmail.com>
2020-05-07 12:43:52 +0530

    final

---
+++ users/config/keys.js
@@ -0,0 +1,5 @@
+// dbPassword = 'mongodb+srv://utkarsh'+ encodeURIComponent('hello%40123') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+module.exports = {
+    mongoURI: 'mongodb+srv://utkarsh:hello%40123@cluster0-irwge.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+};
